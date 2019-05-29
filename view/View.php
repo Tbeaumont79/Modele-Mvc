9b7028a58ff5645ee8930a616c1a7778103b01e6
tbeaumont <tbeaumont79@icloud.com>
2019-05-29 12:00:10 -0400

    set up the view class and and the build content for the index page

--- view/View.php
+++ view/View.php
@@ -12,6 +12,41 @@
       return $this->htmlElement;
     }
 
-  }
+    public function buildHeaderPage() {
+      $this->htmlElement = '<!DOCTYPE html>';
+      $this->htmlElement .= '<html lang="en" dir="ltr">';
+      $this->htmlElement .= '<head><meta charset="utf-8">';
+      $this->htmlElement .= '<title>'.$this->title.'</title>';
+      $this->htmlElement .= '</head><body>';
+    }
+    public function buildContentPage() {
+      $this->htmlElement .= '';
+    }
 
+    public function buildFooterPage() {
+      $this->htmlElement .= '</body></html>';
+    }
+
+    public function buildMenuPage() {
+      $menuArray = [
+        'index.php' => 'Accueil',
+        'about.php' => 'About',
+        'contact.php' => 'Contact'
+      ];
+      $this->htmlElement .= '<nav>';
+      $this->htmlElement .= '<ul>';
+      foreach ($menuArray as $key => $value) {
+        $this->htmlElement .= '<li><a href='.$key.'>'.$value.'</a></li>';
+      }
+      $this->htmlElement .= '</ul>';
+      $this->htmlElement .= '</nav>';
+    }
+
+    public function buildPage() {
+      $this->buildHeaderPage();
+      $this->buildMenuPage();
+      $this->buildContentPage();
+      $this->buildFooterPage();
+    }
+  }
  ?>
--- view/ViewIndex.php
+++ view/ViewIndex.php
@@ -2,13 +2,20 @@
   class ViewIndex extends View {
 
     private $content;
+    private $baliseTitle;
 
     public function __construct($content) {
       $this->content = $content;
+      $this->$baliseTitle = '';
     }
 
     public function buildContent() {
-      $this->htmlElement .= '';
+      $this->htmlElement .= $this->$baliseTitle;
+      $this->htmlElement .= '<section>';
+      $this->htmlElement .= '<div id="content">';
+      $this->htmlElement .= $this->content;
+      $this->htmlElement .= '</div>';
+      $this->htmlElement .= '</section>';
     }
   }
 
